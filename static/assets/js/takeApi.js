dc1d1fb307612ab49c042c22be9a12b6af65aac4
DahyeonKang <kl1223k@naver.com>
2022-07-13 05:28:09 +0900

    # 9 : change the design
    * change API

--- static/assets/js/takeApi.js
+++ static/assets/js/takeApi.js
@@ -1,10 +1,10 @@
-function takeApi(ng, dn) {
+function takeApi(sh, ng) {
     var val = document.getElementById('recentArea_btn').options[document.getElementById("recentArea_btn").selectedIndex].value;
-    console.log(ng);
+
+    var sh = $(sh);
     // var ng = ng;
     // var dn = dn;
-    // var sh = sh;
-    // var aas = aas;
+    var aas = $(aas);
     // var ad = ad;
     // var ym = ym;
     // var ys = ys;
@@ -12,13 +12,13 @@ function takeApi(ng, dn) {
     // var wh = wh;
 
     if (val == 'X') document.getElementById('result').innerHTML = '';
-    if (val == '능곡') document.getElementById('result').innerText = ng;
+    if (val == '능곡') document.getElementById('result').innerHTML = $(ng);
     if (val == '둔내') document.getElementById('result').innerHTML = dn;
-    if (val == '시화') document.getElementById('result').innerHTML = ${sh};
+    if (val == '시화') document.getElementById('result').innerHTML = sh;
     if (val == '아산') document.getElementById('result').innerHTML = aas;
-    if (val == '안덕') document.getElementById('result').innerText = ${ad};
+    if (val == '안덕') document.getElementById('result').innerHTML = $(ad);
     if (val == '약목') document.getElementById('result').innerHTML = ym;
-    if (val == '연서') document.getElementById('result').innerHTML = '<div>{{ys | safe}}</div>';
-    if (val == '왜관') document.getElementById('result').innerHTML = '<div>{{wg | safe}}</div>';
-    if (val == '우항') document.getElementById('result').innerHTML = '<div>{{wh | safe}}</div>';
+    if (val == '연서') document.getElementById('result').innerHTML = $(ys);
+    if (val == '왜관') document.getElementById('result').innerHTML = $(wg);
+    if (val == '우항') document.getElementById('result').innerText = $(wh);
 }
