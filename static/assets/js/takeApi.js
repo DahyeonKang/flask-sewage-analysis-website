4e801f3e23f205806ce2052b5d01dfbddcbc2b1b
DahyeonKang <kl1223k@naver.com>
2022-07-13 05:22:05 +0900

    # 9 : change the design
    * change API

--- static/assets/js/takeApi.js
+++ static/assets/js/takeApi.js
@@ -1,5 +1,6 @@
-function takeApi(ng, dn, aas, sh, ad, ym, ys, wg, wh) {
+function takeApi(ng, dn) {
     var val = document.getElementById('recentArea_btn').options[document.getElementById("recentArea_btn").selectedIndex].value;
+    console.log(ng);
     // var ng = ng;
     // var dn = dn;
     // var sh = sh;
@@ -21,30 +22,3 @@ function takeApi(ng, dn, aas, sh, ad, ym, ys, wg, wh) {
     if (val == '왜관') document.getElementById('result').innerHTML = '<div>{{wg | safe}}</div>';
     if (val == '우항') document.getElementById('result').innerHTML = '<div>{{wh | safe}}</div>';
 }
-
-// function takeApi() {
-//     var value = document.getElementById('recentArea_btn').options[document.getElementById("recentArea_btn").selectedIndex].value;
-//     // console.log(dn);
-//     // console.log(ng);
-//
-//     var ng = ${ng};
-//     // var dn = {{dn|safe}};
-//     // var sh = {{sh|safe}};
-//     // var aas = {{aas|safe}};
-//     // var ad = {{ad|safe}};
-//     // var ym = {{ym|safe}};
-//     // var ys = {{ys|safe}};
-//     // var wg = {{wg|safe}};
-//     // var wh = {{wh|safe}};
-//
-//     if (value == 'X') document.getElementById('result').innerHTML = '';
-//     if (val == '능곡') document.getElementById('result').innerText = ng;
-//     if (val == '둔내') document.getElementById('result').innerText = dn;
-//     if (val == '시환') document.getElementById('result').innerHTML = sh;
-//     if (val == '아산') document.getElementById('result').innerHTML = aas;
-//     if (val == '안덕') document.getElementById('result').innerHTML = ad;
-//     if (val == '약목') document.getElementById('result').innerHTML = ym;
-//     if (val == '연서') document.getElementById('result').innerHTML = ys;
-//     if (val == '왜관') document.getElementById('result').innerHTML = wg;
-//     if (val == '우항') document.getElementById('result').innerHTML = wh;
-// }
